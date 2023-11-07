92fe18440973d934f160b784c91e9abac3a0517a
parisadikshit <2018bec034@sggs.ac.in>
2023-11-07 22:49:28 +0530

    Added data for pokemons

--- react-context/src/data.js
+++ react-context/src/data.js
@@ -6,5 +6,12 @@ export const pokemons = [
     {
         id:2,
         name:'bulbasaur'
+    },
+    {
+        id:3,
+        name:'charmander'
+    },{
+        id:2,
+        name:'ivysaur'
     }
 ]
