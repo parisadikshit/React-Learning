d3b7fee5a38dcf569038af6a3783db0a1c7914a9
parisadikshit <2018bec034@sggs.ac.in>
2023-11-10 15:17:34 +0530

    Memoized component

--- memoization/src/App.js
+++ memoization/src/App.js
@@ -1,4 +1,5 @@
-import { useMemo, useState } from "react";
+import { useMemo, useState, createContext,useContext, memo } from "react";
+const ThemeContext = createContext('speaknow');
 const fibonacci = (n) => {
   if(n<=0) return 0;
   if(n===1) return 1;
@@ -7,15 +8,45 @@ const fibonacci = (n) => {
 
 function App() {
   const [number, setNumber] = useState(9);
+  const [name, setName] = useState('');
+  const [artist, setArtist] = useState('');
+  const [theme, setTheme] = useState('speaknow');
   const answer = useMemo(()=>fibonacci(number),[number])
+  const handleChangeTheme = (()=> setTheme(theme === 'speaknow' ? 'midnights': 'speaknow'));
 
   return (
-   <>
-   <h1>useMemo Example</h1>
+   <div>
    <h2>Fibonacci({number}):{answer}</h2>
    <button onClick={()=>setNumber(number+1)}> + </button>
-   </>
+   <h2>-- Memoization Example --</h2>
+   <label>
+    Name {':'}
+    <input value={name} onChange={e=>setName(e.target.value)}/>
+   </label>
+   <label>
+    Favourite Artist {':'}
+    <input value={artist} onChange={e=>setArtist(e.target.value)}/>
+   </label>
+   <button onClick={handleChangeTheme}>Toggle Theme (Now using theme {theme})</button>
+   <ThemeContext.Provider value={theme}>
+   <Greeting name={name}/>
+   </ThemeContext.Provider>
+   </div>
   );
 }
+
+const Greeting = memo(function Greeting({name}){
+  const theme = useContext(ThemeContext);
+  console.log(`Greeting was rendered at ${new Date().toLocaleTimeString()}`)
+  return (
+    <div className={theme}>
+      <ul>
+      <li>Even when a component is memoized, it will still re-render when its own state changes. </li>
+      <li>Even when a component is memoized, it will still re-render when a context that it’s using changes. </li>
+      </ul>
+      <h4>Hello {name && ','} {name}!</h4>
+    </div>
+  )
+})
 
 export default App;
